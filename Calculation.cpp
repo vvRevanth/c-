d5ad3e63880634baa74980b723854dc9a64903a2
V.V.Revanth <118685357+vvRevanth@users.noreply.github.com>
2022-11-21 00:00:05 +0530

    Add files via upload

---
+++ Calculation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout<<"Enter the number"<<endl;
+    int a;
+    int b;
+    cin>>a;
+    cin>>b;
+    int c=(a+b);
+    int w=(a-b);
+    int t=(a*b);
+    int y=(a/b);
+    int u=(a%b);
+    cout<<c<<endl; 
+    cout<<w<<endl;
+    cout<<t<<endl; 
+    cout<<y<<endl;  
+    cout<<u<<endl; 
+}
